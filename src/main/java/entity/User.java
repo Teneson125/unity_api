54d09de175ad77a509954cf12d7e3931612e6281
Manoj <kannamanoj2@gmail.com>
2022-07-16 23:07:11 +0530

    Update User.java

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,43 @@
+package entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+    private String UserId;
+    private String Name;
+    private String Email;
+    private String Status;
+    
+    public String getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(String userId) {
+        UserId = userId;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+}
