5fc5432089595dfaa18253ea29bfdb018271f4e2
Manoj <kannamanoj2@gmail.com>
2022-07-18 10:09:42 +0530

    created user service and controller

---
+++ src/main/java/com/game/unity_api/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.game.unity_api.controller;
+
+import com.game.unity_api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserService userService;
+
+}
---
+++ src/main/java/com/game/unity_api/service/UserService.java
@@ -0,0 +1,12 @@
+package com.game.unity_api.service;
+
+import com.game.unity_api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+}
